caae3accdf7727d893163eb3ecf45b952e4428d3
YlianPaulouskiy <paulouskiyulyan@gmail.com>
2022-06-23 14:43:18 +0300

    addBankClass

--- src/edu/github/exersise14/bank/Bank.java
+++ src/edu/github/exersise14/bank/Bank.java
@@ -1,10 +1,12 @@
 package edu.github.exersise14.bank;
 
 import edu.github.exersise14.Client;
+import edu.github.exersise14.bank.comparators.ClientNotFountException;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Collections;
 import java.util.List;
 
 @NoArgsConstructor
@@ -12,14 +14,28 @@ public class Bank {
 
     @Getter
     @Setter
-    private String name;
-
+    private String tittle;
     private List<Client> clientList;
 
-    public Bank(String name, List<Client> clientList) {
-        this.name = name;
-        this.clientList = clientList;
+    public Bank(String tittle) {
+        this.tittle = this.tittle;
+        this.clientList = Collections.emptyList();
     }
 
-    
+    public void addClient(Client client) {
+        if (client != null) {
+            clientList.add(client);
+        }
+    }
+
+    public Client getClient(String name, int age) {
+        for (Client client : clientList) {
+            if (client.getName().equals(name) && client.getAge() == age) {
+                return client;
+            } else {
+                throw new ClientNotFountException();
+            }
+        }
+        return null;
+    }
 }
